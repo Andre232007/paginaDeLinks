9aca99190a332068b44bd366434f00b58acaa8c0
Andre232007 <165347419+Andre232007@users.noreply.github.com>
2025-07-14 06:17:23 -0300

    oi

---
+++ main.js
@@ -0,0 +1,57 @@
+import profileData from "./profile.json";
+
+document.title = "Links de " + profileData.name + " " + profileData.lastName;
+
+window.onload = function () {
+  const avatarImage = document.querySelector(".card-header__profile-image");
+  const name = document.querySelector(".card-header__name");
+  const lastName = document.querySelector(".card-header__last-name");
+  const description = document.querySelector(".card-header__description");
+
+  const linksList = document.querySelector(".card__links");
+  const socialLinks = document.querySelector(".card__social-links");
+
+  avatarImage.src = profileData.avatar;
+  avatarImage.alt =
+    "Imagem de perfil de " + profileData.name + " " + profileData.lastName;
+
+  lastName.textContent = profileData.lastName;
+  name.textContent = profileData.name;
+  name.appendChild(lastName);
+
+  description.textContent = profileData.description;
+
+  profileData.links.forEach((link) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+
+    a.href = link.href;
+    a.textContent = link.text;
+    a.target = "_blank";
+    li.appendChild(a);
+    linksList.appendChild(li);
+  });
+
+profileData.socialLinks.forEach((socialLink) => {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.href = socialLink.href;
+  a.target = "_blank";
+  a.rel = "noopener noreferrer";
+
+  if (socialLink.inlineSvg && socialLink.svg) {
+    // Se for um SVG inline, injeta diretamente
+   a.innerHTML = `<span class="svg-icon">${socialLink.svg}</span>`;
+
+  } else {
+    // Se for imagem comum, cria um <img>
+    const img = document.createElement("img");
+    img.src = socialLink.icon;
+    img.alt = socialLink.text + " logo";
+    a.appendChild(img);
+  }
+
+  li.appendChild(a);
+  socialLinks.appendChild(li);
+});
+}
